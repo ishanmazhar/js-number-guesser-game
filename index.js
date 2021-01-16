136484f7afdd7f824092b0529b347cc352c98159
ishanmazhar <ishanmazhar2@gmail.com>
2021-01-16 10:53:46 +0600

    5th Commit

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ let startBtn = document.getElementById('start-btn');
     			max = Math.floor(max);
     			return Math.floor(Math.random() * (max - min) + min);
     		}
-   			var rand = getRandomInt(1, 5)
+   			var rand = getRandomInt(1, 10)
 
 			for(let i = 0; i < 3; i++) {
-				var userInput = parseInt(prompt('Input the number from 1 to 5 (inclusive) you have guessed'));
+                var userInput = parseInt(prompt('Input the number from 1 to 10 (inclusive) you have guessed'
+                    + '\n' + "Insert 0 (zero) to end the game")); 
 				console.log(userInput);
 
 				if(isNaN(userInput)){
-					alert('Pleasue insert a number from 1 to 5 inclusive!');
+                    alert('Pleasue insert a number from 1 to 10 inclusive!'
+                    + '\n' + "Insert 0 (zero) to end the game");
 				}
-				if (userInput < rand) {
+				if (userInput < rand && userInput > 0) { 
    					var result = "Correct answer is greater!";
 				   	alert(result);
 				   	console.log(`rand = ${rand}`);
@@ -29,10 +31,15 @@ let startBtn = document.getElementById('start-btn');
 					result = "You win!!";
 					alert(result);
 					return;
+                } 
+                else if (userInput == 0) {
+					result = "Game Terminated!"; 
+					alert(result);
+					return;
 				} 
 			}
 
 			if(userInput < rand || userInput > rand || isNaN(userInput)) {
-				alert("You Lose!");
+				alert("You Lost! Try Again!"); 
 			}
     	}
